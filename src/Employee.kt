64e0caa7fc8e4bf2812c06dd8d55c2deb97cd739
Hajar Salamah <programer4455@gmail.com>
2020-09-13 16:00:07 +0300

    Initial commit

---
+++ src/Employee.kt
@@ -0,0 +1,61 @@
+class Employee private constructor(builder: Builder) {
+    var firstName: String ?
+    var lastName: String ?
+    var id: String ?
+    var managerId: String ?
+    var isManager: Boolean
+    init {
+       this.firstName = builder.firstName
+        this.lastName = builder.lastName
+        this.id = builder.id
+        this. isManager = builder.isManager
+        this.managerId = builder.managerId
+    }
+    class Builder {
+        var firstName: String? = null
+        var lastName: String? = null
+        var id: String? = null
+        var isManager = false
+         var managerId: String? = null
+        fun build(): Employee {
+            return Employee(this)
+        }
+
+        fun setFirstName(firstName: String?): Builder {
+            this.firstName = firstName
+            return this
+        }
+
+        fun setLastName(lastName: String?): Builder {
+            this.lastName = lastName
+            return this
+        }
+
+        fun setId(id: String?): Builder {
+            this.id = id
+            return this
+        }
+
+        fun setIsManager(manager: Boolean): Builder {
+            isManager = manager
+            return this
+        }
+
+        fun setManagerId(managerId: String?): Builder {
+            this.managerId = managerId
+            return this
+        }
+    }
+
+
+}
+fun main() {
+    var employee = Employee.Builder()
+            .setFirstName("Hajar")
+            .setLastName("Salamah")
+            .setManagerId("XXX")
+            .setIsManager(false)
+    println( " your name is :"+employee.firstName+" "+employee.lastName+
+            "\n"+"Are you manager : "+employee.isManager)
+
+}
